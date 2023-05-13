2a4dc8d18dbc51ad38f6cae008a19034033df3a6
Cristian Betivu <cbe@languagewire.com>
2023-05-13 23:22:04 +0300

    More fiddling with repo creation

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,20 @@ struct Config {
     branch_name: String,
 }
 
-fn get_positional_arg(args: &Vec<String>, index: usize) -> Result<String, Box<dyn Error>> {
-    let position = index + 1;
-    let arg = args
-        .get(index)
-        .ok_or(format!("expected argument at position {position}"))?
-        .to_owned();
-
-    return Ok(arg);
-}
-
 impl Config {
     fn build(args: Vec<String>) -> Result<Self, Box<dyn Error>> {
-        let branch_name = get_positional_arg(&args, 0)?;
+        let branch_name = Config::get_positional_arg(&args, 0)?;
         return Ok(Self { branch_name });
+    }
+
+    fn get_positional_arg(args: &Vec<String>, index: usize) -> Result<String, Box<dyn Error>> {
+        let position = index + 1;
+        let arg = args
+            .get(index)
+            .ok_or(format!("expected argument at position {position}"))?
+            .to_owned();
+
+        return Ok(arg);
     }
 }
 
@@ -38,25 +38,37 @@ fn get_repo() -> Result<Repository, git2::Error> {
         let tree_oid = index.write_tree()?;
         let tree = repo.find_tree(tree_oid)?;
 
+        let parent_commit = match repo.revparse_single("HEAD") {
+            Ok(obj) => Some(obj.into_commit().unwrap()),
+            // First commit so no parent commit
+            Err(e) if e.code() == git2::ErrorCode::NotFound => None,
+            Err(e) => return Err(e),
+        };
+
+        let mut parents = Vec::new();
+        if parent_commit.is_some() {
+            parents.push(parent_commit.as_ref().unwrap());
+        }
+
         let signature = repo.signature()?;
-        // repo.commit_signed("is this the message?", &signature.name().unwrap(), None)?;
         repo.commit(
             Some("HEAD"),
             &signature,
             &signature,
             "My message",
             &tree,
-            &[],
+            &parents,
         )?;
-    }
+        index.write()?;
+    } // HACK: Is this needed due to a bad lifetime annotation?
 
-    Ok(repo)
+    return Ok(repo);
 }
 
 fn helper(config: &Config) -> Result<(), git2::Error> {
     let repo = get_repo()?;
     let commit = repo.find_commit(repo.head()?.peel_to_commit()?.id())?;
-    let _branch = repo.branch(&config.branch_name, &commit, false)?;
+    let _branch = repo.branch(&config.branch_name, &commit, true)?;
 
     Ok(())
 }
