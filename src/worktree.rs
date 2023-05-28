7789a2481d88ca9a0f19308eb9ef686121dfee2c
Cristian Betivu <cbe@languagewire.com>
2023-05-28 12:53:37 +0300

    Actually create a worktree

--- src/worktree.rs
+++ src/worktree.rs
@@ -1,13 +1,13 @@
-use git2::Repository;
-use std::fs as std_fs;
-use std::{error::Error, path::PathBuf};
-
 use self::fs::CopyTargets;
+use git2::{BranchType, Reference, Repository, WorktreeAddOptions};
+use std::{error::Error, path::PathBuf};
 
 mod fs;
 
-pub fn get_repo() -> Result<Repository, git2::Error> {
-    let path = "/tmp/dummy_repo";
+const MASTER_BRANCH: &str = "master";
+
+pub fn get_repo() -> Result<Repository, Box<dyn Error>> {
+    let path = std::env::current_dir()?;
     let repo = Repository::init(path)?;
 
     Ok(repo)
@@ -21,24 +21,45 @@ pub fn create_worktree(repo: &Repository, branch_name: &str) -> Result<(), Box<d
 }
 
 fn new_worktree(repo: &Repository, branch_name: &str) -> Result<PathBuf, Box<dyn Error>> {
-    // TODO: Actually invoke git to create a worktree
-    // For now it will just create the folder path based on the branch name
-    // TODO: Probably good to validate the branch name does not try to traverse the fs
+    if !Reference::is_valid_name(branch_name) {
+        return Err(format!("Branch name '{branch_name}' is not valid").into());
+    }
 
-    let commit = repo.find_commit(repo.head()?.peel_to_commit()?.id())?;
-    let _branch = repo.branch(branch_name, &commit, true)?;
+    let mut worktree_add_options = WorktreeAddOptions::new();
 
+    // TODO: Maybe support picking a different source ref
+    let ref_branch = repo.find_branch(MASTER_BRANCH, BranchType::Local)?;
+    let new_branch = repo.branch(branch_name, &ref_branch.get().peel_to_commit()?, false)?;
+    worktree_add_options.reference(Some(new_branch.get()));
+
+    // let mut path = env::current_dir()?;
     let repo_root = get_root_path(repo)?;
-    let path = repo_root.join(branch_name);
-    std_fs::create_dir_all(path.clone())?;
+    let worktree_path = repo_root.join(branch_name); // TODO: Perhaps split by '/' and then join parts to path
 
-    Ok(path)
+    // worktree name is used to create folder .git/worktrees/<name>
+    let worktree_name = branch_name.replace(std::path::MAIN_SEPARATOR, "_");
+
+    let worktree = repo.worktree(
+        &worktree_name,
+        worktree_path.as_path(),
+        Some(&worktree_add_options),
+    )?;
+
+    assert_eq!(worktree_path, worktree.path().to_path_buf());
+
+    Ok(worktree_path)
 }
 
 fn prepare_worktree(repo: &Repository, target_dir: PathBuf) -> Result<(), Box<dyn Error>> {
     let repo_root = get_root_path(repo)?;
 
-    let source_dir = repo_root.join("master");
+    let source_dir = repo_root.join(MASTER_BRANCH);
+    let source_dir = if source_dir.exists() {
+        source_dir
+    } else {
+        repo_root
+    };
+
     let targets = [
         CopyTarget::new("**REMOVED**".to_string(), true),
         CopyTarget::new("**REMOVED**".to_string(), true),
