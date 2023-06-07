2426bdddd9470c97b924d102e7af97992b9de70d
Cristian Betivu <cbe@languagewire.com>
2023-06-07 22:00:40 +0300

    Parse args using clap

---
+++ src/cli.rs
@@ -0,0 +1,65 @@
+use clap::{command, error::Error, Arg, ArgMatches, Args, Command, FromArgMatches, Parser};
+
+#[derive(Debug)]
+struct CliArgs {
+    pub remote_branch: Option<String>,
+}
+
+const REMOTE_BRANCH_NAME: &str = "REMOTE_BRANCH";
+
+impl FromArgMatches for CliArgs {
+    fn from_arg_matches(matches: &ArgMatches) -> Result<Self, Error> {
+        let mut matches = matches.clone();
+        Self::from_arg_matches_mut(&mut matches)
+    }
+
+    fn from_arg_matches_mut(matches: &mut ArgMatches) -> Result<Self, Error> {
+        Ok(Self {
+            remote_branch: matches.remove_one::<String>(REMOTE_BRANCH_NAME),
+        })
+    }
+
+    fn update_from_arg_matches(&mut self, matches: &ArgMatches) -> Result<(), Error> {
+        let mut matches = matches.clone();
+        self.update_from_arg_matches_mut(&mut matches)
+    }
+
+    fn update_from_arg_matches_mut(&mut self, matches: &mut ArgMatches) -> Result<(), Error> {
+        if let Some(remote_branch) = matches.remove_one::<String>(REMOTE_BRANCH_NAME) {
+            self.remote_branch = Some(remote_branch);
+        }
+        Ok(())
+    }
+}
+
+impl Args for CliArgs {
+    fn augment_args(cmd: Command) -> Command {
+        Self::augment_args_for_update(cmd)
+    }
+
+    fn augment_args_for_update(cmd: Command) -> Command {
+        cmd.arg(
+            Arg::new(REMOTE_BRANCH_NAME)
+                .short('r')
+                .long("remote")
+                .help("The remote branch to check out, if any. If supplied without a value (i.e. a flag), then it will default to branch_name")
+                .num_args(0..=1)
+                .require_equals(true)
+                // https://docs.rs/clap/latest/clap/struct.Arg.html#method.default_missing_value
+                // allows using the argument both as a flag and as an argument
+                .default_missing_value(""),
+        )
+    }
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct CliConfig {
+    /// the name of the branch to create
+    pub branch_name: String,
+
+    // https://docs.rs/clap/latest/clap/_derive/index.html#flattening-hand-implemented-args-into-a-derived-application
+    // default_missing_value is only supported via the Arg struct
+    #[command(flatten)]
+    more_args: CliArgs,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,11 @@
-use std::env;
+use clap::Parser;
+use cli::CliConfig;
+use color_eyre::eyre::Result;
 
-use color_eyre::{eyre::Result, Report};
-
+mod cli;
 mod worktree;
 
-#[derive(Debug)]
-struct Config {
-    branch_name: String,
-}
-
-impl Config {
-    fn build(args: Vec<String>) -> Result<Self> {
-        let branch_name = Config::get_positional_arg(&args, 0)?;
-        Ok(Self { branch_name })
-    }
-
-    fn get_positional_arg(args: &[String], index: usize) -> Result<String> {
-        let position = index + 1;
-        let arg = args
-            .get(index)
-            .ok_or(Report::msg(format!(
-                "expected argument at position {position}"
-            )))?
-            .to_owned();
-
-        Ok(arg)
-    }
-}
-
-fn helper(config: &Config) -> Result<()> {
+fn helper(config: &CliConfig) -> Result<()> {
     let repo = worktree::get_repo()?;
     worktree::create_worktree(&repo, &config.branch_name)?;
 
@@ -37,8 +14,8 @@ fn helper(config: &Config) -> Result<()> {
 
 fn main() -> Result<()> {
     color_eyre::install()?;
-    let args: Vec<_> = env::args().skip(1).collect();
-    let config = Config::build(args).unwrap();
+    let config = CliConfig::parse();
+
     helper(&config)?;
 
     Ok(())
