910a455f898aeb419d20bffb53bd7be63412b231
Cristian Betivu <cbe@languagewire.com>
2023-06-07 22:49:08 +0300

    Generate shell completion on build

---
+++ build.rs
@@ -0,0 +1,18 @@
+use clap::CommandFactory;
+use clap::ValueEnum;
+use clap_complete::{generate_to, Shell};
+use std::env;
+
+include!("src/cli.rs");
+
+fn main() -> Result<(), Error> {
+    let outdir = env::var_os("OUT_DIR").expect("expected to get output dir path");
+
+    let mut cmd = CliConfig::command();
+    let cmd_name = cmd.get_name().to_string();
+    for &shell in Shell::value_variants() {
+        generate_to(shell, &mut cmd, &cmd_name, outdir.clone())?;
+    }
+
+    Ok(())
+}
