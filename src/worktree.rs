7f82837eb2c7693b65f4903499b37f4845af2290
Cristi <cbetivu@gmail.com>
2024-03-30 15:44:26 +0200

    Fix detection of the repository root path; allow running command from any folder (aside submodules)

--- src/worktree.rs
+++ src/worktree.rs
@@ -1,5 +1,5 @@
 use color_eyre::eyre::{Context, Result};
-use color_eyre::Report;
+use color_eyre::{Report, Section};
 use git2::{Branch, BranchType, Repository, WorktreeAddOptions};
 use std::fs;
 use std::path::PathBuf;
@@ -9,8 +9,7 @@ use crate::tasks::{files, shell};
 use crate::tasks::{ProjectConfig, Task};
 
 pub fn get_repo_curr_dir() -> Result<Repository> {
-    let path = std::env::current_dir()?;
-    let repo = Repository::open(path)?;
+    let repo = Repository::open_from_env()?;
 
     Ok(repo)
 }
@@ -66,6 +65,19 @@ fn new_worktree(
     ref_branch: &Branch,
     remote: Option<&str>,
 ) -> Result<PathBuf> {
+    let repo_root = get_root_path(repo)?;
+    // convert "/" to OS specific path separator
+    let worktree_path = repo_root.join(branch_name.split('/').collect::<PathBuf>());
+
+    // must create parent directory for nested folders
+    let parent_dir = worktree_path
+        .parent()
+        .ok_or_else(|| Report::msg("expected to extract parent dir"))?;
+    fs::create_dir_all(parent_dir).context(format!(
+        "Failed to create directory {}",
+        parent_dir.display()
+    ))?;
+
     let mut new_branch = repo
         .branch(branch_name, &ref_branch.get().peel_to_commit()?, false)
         .wrap_err("Failed to create new branch")?;
@@ -80,27 +92,16 @@ fn new_worktree(
     let mut worktree_add_options = WorktreeAddOptions::new();
     worktree_add_options.reference(Some(new_branch.get()));
 
-    let repo_root = get_root_path(repo)?;
-    // convert "/" to OS specific path separator
-    let worktree_path = repo_root.join(branch_name.split('/').collect::<PathBuf>());
-
-    let parent_dir = worktree_path
-        .parent()
-        .expect("expected to extract parent dir");
-
-    fs::create_dir_all(parent_dir).context(format!(
-        "Failed to create directory {}",
-        parent_dir.display()
-    ))?;
-
     // worktree name is used to create directory .git/worktrees/<name>
     let worktree_name = branch_name.replace(std::path::MAIN_SEPARATOR, "_");
 
-    let worktree = repo.worktree(
-        &worktree_name,
-        worktree_path.as_path(),
-        Some(&worktree_add_options),
-    )?;
+    let worktree = repo
+        .worktree(
+            &worktree_name,
+            worktree_path.as_path(),
+            Some(&worktree_add_options),
+        )
+        .with_context(|| "failed to create worktree")?;
 
     assert_eq!(worktree_path, worktree.path().to_path_buf());
 
@@ -134,7 +135,41 @@ fn prepare_worktree(repo: &Repository, target_dir: PathBuf, config: &ProjectConf
 }
 
 fn get_root_path(repo: &Repository) -> Result<PathBuf> {
-    let repo_root = repo.path().parent().map(|f| f.to_path_buf());
-
-    repo_root.ok_or(Report::msg("Failed to get root path"))
+    if repo.is_worktree() {
+        let workdir = repo.workdir().unwrap(); // /tmp/repo/feature/nested/
+        let worktree_path = repo.path(); // /tmp/repo/.bare/worktrees/nested/
+        let mut root_path = PathBuf::new();
+        for component in worktree_path.iter() {
+            root_path.push(component);
+            if !workdir.starts_with(&root_path) {
+                break;
+            }
+        }
+        let repo_root = root_path.parent().map(|f| f.to_path_buf());
+        repo_root
+            .ok_or_else(|| Report::msg("Failed to get root path from worktree workdir"))
+            .with_note(|| {
+                format!(
+                    "workdir: {}, worktree: {}, root path: {}",
+                    workdir.display(),
+                    worktree_path.display(),
+                    root_path.display()
+                )
+            })
+    } else if repo.is_bare() {
+        // repo.path(): /tmp/repo/.bare/
+        let repo_root = repo.path().parent().map(|f| f.to_path_buf());
+        repo_root
+            .ok_or_else(|| Report::msg("Failed to get root path from bare repository"))
+            .with_note(|| format!("repo path: {}", repo.path().display()))
+    } else {
+        Err(Report::msg("Not a worktree or bare repo").with_note(|| {
+            format!(
+                "Command can be only from a bare repository or a work tree. Work directory has been identified to: {}",
+                repo.workdir()
+                    .map(|p| p.display().to_string())
+                    .unwrap_or("None".to_owned())
+            )
+        }))
+    }
 }
